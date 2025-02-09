2fd0414676c086a8787c114e0d2e27a210096a7c
neeraj@1704 <neerajkhalkar@gmail.com>
2025-02-09 22:44:04 +0530

    Object Type defination

--- src/ObjectTypes.ts
+++ src/ObjectTypes.ts
@@ -3,18 +3,33 @@ const car : { type : string ,model : string, year : number } = {
     model : "Corola",
     year : 2008
 }
-
 console.log(car)
 
 
-
+//  brand object definition 
 const brands : { brand : string , model : string , engine : string, drive : number} = {
     brand : "BMW",
     model : "M5CS",
     engine : "V12",
     drive : 4
 }
-
 console.log(brands)
 console.log(brands.brand);
+
+
+// Type Inference
+// here we are updating the object value
+const brands1 = {
+    type : "Mercedes" 
+};
+car.type = "ford";
+
+
+// Optional Property 
+// ?: : Optional Parameter
+const cars : {type :string, mileage ?: number} = {
+    type : "Sedan"
+}
+
+console.log(cars)
 
