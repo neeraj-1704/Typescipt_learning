7307a1fe50a6c0f3c1f56153bca4f16e2e6d9d90
neeraj@1704 <neerajkhalkar@gmail.com>
2025-02-09 22:32:42 +0530

    Object Defination and the Initilization

---
+++ src/ObjectTypes.ts
@@ -0,0 +1,20 @@
+const car : { type : string ,model : string, year : number } = {
+    type : "Toyata",
+    model : "Corola",
+    year : 2008
+}
+
+console.log(car)
+
+
+
+const brands : { brand : string , model : string , engine : string, drive : number} = {
+    brand : "BMW",
+    model : "M5CS",
+    engine : "V12",
+    drive : 4
+}
+
+console.log(brands)
+console.log(brands.brand);
+
--- src/arrayTypes.ts
+++ src/arrayTypes.ts
@@ -1,24 +1,48 @@
-const names : string[]  = []  ;       // defining the array 
+// const names : string[]  = []  ;       // defining the array 
 
-names.push('neeraj');
-names.push('sagar');
-names.push('Prathemesh');
-
-
-for( let i =0 ;i <names.length; i++){      
-    console.log(names[i]);
-}
+// names.push('neeraj');
+// names.push('sagar');
+// names.push('Prathemesh');
 
 
-// defing the read only array in the js
-const name1 : readonly string[] = [];
+// for( let i =0 ;i <names.length; i++){      
+//     console.log(names[i]);
+// }
 
-// wroung way to defined the array in the ts
-// const number  = [1,2,4,5];
+
+// // defing the read only array in the js
+// const name1 : readonly string[] = [];
+
+// // wroung way to defined the array in the ts
+// // const number  = [1,2,4,5];
+// // number.push(3);
+// // number.push('3');
+
+// // right way to defined the array and pushing the element
+// const number : any = [1,2,4,5];
 // number.push(3);
 // number.push('3');
 
-// right way to defined the array and pushing the element
-const number : any = [1,2,4,5];
-number.push(3);
-number.push('3');
+
+
+// // Typed Arrays
+// // A tuple is a typed array with a pre-defined length and types for each index.
+
+// // Tuples are great because they allow each element in the array to be a known type of value.
+
+// // To define a tuple, specify the type of each element in the array:
+
+// // ExampleGet your own TypeScript Server
+// // define our tuple
+// let ourTuple: [number, boolean, string];
+
+// // initialize correctly
+// ourTuple = [5, false, 'Coding God was here'];
+// // As you can see we have a number, boolean and a string. But what happens if we try to set them in the wrong order:
+
+// // Example
+// // define our tuple
+// let ourTuple1: [number, boolean, string];
+
+// // initialized incorrectly which throws an error
+// ourTuple1 = [false, 'Coding God was mistaken', 5];
